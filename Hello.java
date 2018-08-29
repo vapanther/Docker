dd251379164c4ae964998d90425b52fde84ab1f3
Ankit Kumar Tripathi <Ankit.Tripathi@rsystems.com>
2018-08-29 12:52:18 +0530

    Docker file with the java simple and salary application

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello{  
+public static void main(String[] args){  
+System.out.println("This is java app by using Docker");
+while(true)
+{
+System.out.println("Created by :- Ankit");
+}  
+}  
+}  
